c4efe78a591f34b28f7baa973c8aa3fb9a421ec3
dtk42 <dtk42@CIT-PHILLIPS-21.cornell.edu>
2016-12-07 03:20:49 -0500

    NEW YAS Email me dtk42 with questions

--- runMe.m
+++ runMe.m
@@ -11,16 +11,21 @@ side2Dim = [ 10 10 5 ]/100;
 side2Mass = 4*(0.5/3);
 
 %% Tether
-spring1L = 1;
+spring1L = 0;
 spring1ks = 1;
-spring1kd = 0.1;
+spring1kd = 1;
 
-spring2L = 1;
+spring2L = 0;
 spring2ks = 1;
-spring2kd = 0.1;
+spring2kd = 1;
+
+%% Torque
+torY = 0.00001;
+torX = 0.0000001;
+
 
 %%
-tend = 10;
+tend = 10000;
 sim('main');
 
 
